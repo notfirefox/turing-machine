d213c6a3a548c644282ed36b47f3d1ca4a64f4ab
notfirefox <138280734+notfirefox@users.noreply.github.com>
2023-09-29 14:34:48 +0200

    Update formatting

--- src/main.c
+++ src/main.c
@@ -5,14 +5,14 @@
 #include "turing.h"
 
 int main() {
-  char *word = NULL;
-  printf("Enter word: ");
-  scanf("%ms", &word);
+    char *word = NULL;
+    printf("Enter word: ");
+    scanf("%ms", &word);
 
-  struct turingmachine *tm = create_turingmachine(accept1, delta1);
-  process_word(tm, word);
-  delete_turinmachine(tm);
+    struct turingmachine *tm = create_turingmachine(accept1, delta1);
+    process_word(tm, word);
+    delete_turinmachine(tm);
 
-  free(word);
-  return 0;
+    free(word);
+    return 0;
 }
