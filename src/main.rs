d151bad73ee0365b05ff3c93487864801035fb06
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-01-21 17:09:54 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{self, BufRead}};
+use std::io::{self, BufRead};
 
 pub mod tm1;
 pub mod turing;
@@ -7,19 +7,14 @@ use crate::tm1::accept;
 use crate::tm1::delta;
 
 fn parse_word(prompt: &str) -> Option<String> {
-    println!("{}: ", prompt);
-    let mut iterator = io::stdin().lock().lines();
-    let line = match iterator.next()? {
-        Ok(x) => x,
-        Err(_) => return None,
-    };
-    Some(line.to_string())
+    println!("{prompt}: ");
+    io::stdin().lock().lines().next()?.ok()
 }
 
 fn main() {
     if let Some(word) = parse_word("Enter word") {
         let accept = Box::new(|x| accept(&x));
-        let delta = Box::new(|x: &_| delta(&x));
+        let delta = Box::new(|x: &_| delta(x));
 
         let mut tm = turing::TuringMachine::new(accept, delta);
         tm.process_word(&word);
