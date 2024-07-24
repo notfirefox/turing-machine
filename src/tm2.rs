9dcca66a446fe2bc20dad74319fa561531280fa0
notfirefox <138280734+notfirefox@users.noreply.github.com>
2024-07-24 18:40:27 +0200

    Update turing machine two

--- src/tm2.rs
+++ src/tm2.rs
@@ -2,7 +2,7 @@ use crate::turing::{DeltaResult, Move::Left, Move::Right, BLANK};
 
 #[must_use]
 pub const fn accept2(state: usize) -> bool {
-    state == 3
+    state == 4
 }
 
 #[must_use]
@@ -10,8 +10,10 @@ pub const fn delta2(state: usize, input: char) -> Option<DeltaResult> {
     let (state, output, r#move) = match (state, input) {
         (0, '0') => (1, '1', Right),
         (1, '1') => (2, '0', Left),
-        (1, BLANK) => (3, BLANK, Right),
         (2, '1') => (0, '1', Right),
+        (1, BLANK) => (3, BLANK, Left),
+        (3, '1') => (3, '1', Left),
+        (3, BLANK) => (4, BLANK, Right),
         (_, _) => return None,
     };
     Some(DeltaResult::new(state, output, r#move))
